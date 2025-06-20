d255b8f70e7ddb615d13159d26ebfcb2f216a28e
Vicky1jat <vicky1jat6376831267@gmail.com>
2025-06-20 16:31:48 +0530

    funtionality for increasing,decring and remove things in cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = parseFloat(item.cost.substring(1)); // remove "$" and convert to number
+      total += cost * quantity;
+    });
+
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault(); // Optional: prevents default behavior like page reload
+    if (onContinueShopping) {
+      onContinueShopping(e); // call the function passed from the parent
+    }
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity -1 }));
+    }
+    else{
+      dispatch(removeItem(item.name));
+    }
   };
 
+  
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
